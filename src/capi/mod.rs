5da852d5fbd12cf8e8bc0dba426ca92c238abac5
Chris Couzens <ccouzens@gmail.com>
2019-06-30 20:57:35 +0100

    Use crates that generate the c bindings
    This avoids having to do the bindings a 2nd time in this project.

--- src/capi/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Raw (unsafe) C API binding for Leptonica and Tesseract
-
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(improper_ctypes)]
-
-include!("./leptonica-tesseract.rs");
