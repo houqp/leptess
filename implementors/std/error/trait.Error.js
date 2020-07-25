0e887b4f7a3afff48b1b0ae16856b65e44cc15a3
QP CircleCI docs build <dave2008713@gmail.com>
2020-07-25 18:17:57 +0000

    auto commit from build [ci skip]

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["leptess"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"leptess/tesseract/struct.TessInitError.html\" title=\"struct leptess::tesseract::TessInitError\">TessInitError</a>",synthetic:false,types:["leptess::tesseract::TessInitError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
